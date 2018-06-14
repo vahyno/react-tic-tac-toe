87a66105c99f6650af47762a311391c3244fb4c0
vahyno <vahyno@gmail.com>
2018-06-14 00:31:43 -0700

    players taking turns

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,12 +4,16 @@ import Square from './Square';
 class Board extends Component {
   state = {
     squares: Array(9).fill(null),
+    xIsNext: true,
   };
 
   handleClick = (i) => {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare = (i) => {
@@ -20,7 +24,7 @@ class Board extends Component {
     }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext? 'X' : 'O' }`;
 
     return (
       <div>
